f792be4a74361d406f1e051f597391cf61ec4f77
Ernest Wambua <ernestwambua2@gmail.com>
2021-08-06 09:16:03 +0300

    hamming distance

---
+++ HammingDistance/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String string1, string2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter String 1:");
+        string1 = input.nextLine();
+
+        System.out.println("Enter String 2:");
+        string2 = input.nextLine();
+
+        if(string1.length() != string2.length()) {
+            System.out.println("The two strings should be of equal length");
+        } else {
+            System.out.println("Hamming Distance = " + hammingDistance(string1, string2));
+        }
+
+        String string3;
+        int hammingDistance;
+
+        System.out.println("Enter String:");
+        string3 = input.nextLine();
+        System.out.println("Enter Hamming Distance:");
+        hammingDistance = input.nextInt();
+
+
+
+    }
+
+    public static int hammingDistance(String string1, String string2) {
+        int count = 0;
+
+        for(int i = 0; i < string1.length(); i++) {
+            if (string1.charAt(i) != string2.charAt(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
